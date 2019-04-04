c4810d33f5fba4d5fcb2ced6aa22e038cf0071ec
ratapongonjun <ratapongonjun@hotmail.com>
2019-04-04 21:27:21 +0700

    14fabSearch

--- app/src/main/java/com/example/boomapp/MainActivity.java
+++ app/src/main/java/com/example/boomapp/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.boomapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,6 +34,15 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
+        final ConstraintLayout ctMain = findViewById(R.id.ctMain);
+
+        FloatingActionButton fabSearch = findViewById(R.id.fabSearch);
+        fabSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(ctMain,"ข้อความ Snackbar มาจากกดปุ่ม FAB",Snackbar.LENGTH_LONG).show();
+            }
+        });
 
     }
 }
