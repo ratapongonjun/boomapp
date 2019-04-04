0699d9f1e61fc4f511e438931a1d3ee2cc8964c2
ratapongonjun <ratapongonjun@hotmail.com>
2019-04-04 15:40:54 +0700

    11InputTextAndShowTextAfterClickBUTTON

--- app/src/main/java/com/example/boomapp/MainActivity.java
+++ app/src/main/java/com/example/boomapp/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.boomapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText edtFullName = findViewById(R.id.edtFullName);
+        Button cmdCheck = findViewById(R.id.cmdCheck);
+
+        if (cmdCheck != null){
+            cmdCheck.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (edtFullName.getText().toString().trim() != ""){
+                        Toast.makeText(MainActivity.this,edtFullName.getText(),Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+
+        }
+
+
     }
 }
